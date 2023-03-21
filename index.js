2ad39f8fefcc789a39b9c122b6bf2c04e693d445
Ronald Barasa <barasaogoro@gmail.com>
2023-03-21 09:44:16 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 //Student Grade Generator//
-marks = int(input("Enter student marks (0-100): "))
+let marks = prompt("Please enter the student's marks (0-100):");
 
-if (marks > 79):
-    grade = 'A'
-elif 60 <= marks <= 79:
-    grade = 'B'
-elif 50 <= marks <= 59:
-    grade = 'C'
-elif 40 <= marks <= 49:
-    grade = 'D'
-else:
-    grade = 'E'
-
-print(f"The student's grade is: {grade}")
+if (marks > 100 || marks < 0) {
+  console.log("Invalid input. Marks should be between 0 and 100.");
+} else if (marks >= 80) {
+  console.log("Grade: A");
+} else if (marks >= 60) {
+  console.log("Grade: B");
+} else if (marks >= 50) {
+  console.log("Grade: C");
+} else if (marks >= 40) {
+  console.log("Grade: D");
+} else {
+  console.log("Grade: E");
+}
